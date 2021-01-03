e94a5e948bf618f5edfc90bd080770de7d658b0c
Jason Kao <jason.kao@columbia.edu>
2021-01-03 15:32:34 -0500

    app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-function App() {
-  return (
-    <div>
-      Hello world
-    </div>
-  );
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hi my name is xia luo te.</p>
+        <p>
+          Hi am Charlotte, a third-year computer science-math major at Columbia
+          University I also fuck up the art world: you can view my art{' '}
+          <a href="#">here</a>. My resume is <a href="#">here</a>. You can hmu
+          at <a href="mailto:li.zihao@columbia.edu">li.zihao@columbia.edu</a>.
+        </p>
+        <p>
+          I can do data processing, mapping, natural language processing,
+          design, video editing, data visualization.
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
