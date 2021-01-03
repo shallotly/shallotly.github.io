46a87aab61726e1f663692f7be3571d25a2a2b74
Jason Kao <jason.kao@columbia.edu>
2021-01-03 16:14:10 -0500

    done bitch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
 import { Component } from 'react';
 import styled from 'styled-components';
 
-const Container = styled.p`
-p {
+const Container = styled.div`
+  margin-top: 40px;
   font-family: Helvetica;
-  max-width: 600px;
   color: #121212;
   font-size: 18px;
-}
-`
+
+  > p {
+    max-width: 600px;
+    margin: 15px auto;
+  }
+
+  h1 {
+    font-size: 18px;
+    border-bottom: 1px solid #ddd;
+    padding: 0 5px 6px;
+    display: inline-block;
+    margin: 20px 0;
+    margin-left: 50%;
+    transform: translateX(-50%);
+  }
+`;
+
+const Footer = styled.div`
+  margin: 0 auto;
+  margin-top: 12px;
+  display: flex;
+  justify-content: center;
+
+  p {
+    display: inline-block;
+    margin: 0;
+  }
+
+  a {
+    color: black;
+  }
+
+  p:not(:last-child) {
+    border-right: 2px solid #121212;
+    padding-right: 7px;
+    margin-right: 7px;
+  }
+`;
+
+const footerLinks = {
+  GitHub: 'https://github.com/shallotly',
+  Twitter: 'https://twitter.com/shallotly',
+  Resume: '#',
+  Email: 'mailto:li.zihao@columbia.edu',
+};
+
 class App extends Component {
   render() {
     return (
@@ -24,6 +67,17 @@ class App extends Component {
           I can do data processing, mapping, natural language processing,
           design, video editing, data visualization.
         </p>
+        <h1>Projects</h1>
+        <h1>Coursework</h1>
+        <Footer>
+          {Object.keys(footerLinks).map(key => (
+            <p key={key}>
+              <a key={key} href={footerLinks[key]}>
+                {key}
+              </a>
+            </p>
+          ))}
+        </Footer>
       </Container>
     );
   }
