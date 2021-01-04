f01330c7c76d20641225fd5c4f0490d49094bdbc
Charlotte Li <li.zihao@columbia.edu>
2021-01-03 20:26:20 -0500

    reduced descpt length

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ export default {
             publisher: 'NBC News',
             articleUrl: 'https://www.nbcnews.com/news/us-news/map-trump-international-student-rule-effects-state-n1233219',
             publisherUrl: 'https://www.nbcnews.com/pages/author/charlotte-li',
-            description: 'This story was published in response to ICE’s policy regarding international visa and immigration status amid COVID-19 and school reopening. For this story, I worked with Jiachuan Wu, my colleague at NBC News over the summer, to scrape college tuition data and F and J visa data over the years. We processed these data in a spreadsheet. I also worked on making the map and the bar chart in illustrator and D3geo. Jiachuan added a toggle to the maps.',
+            description: 'This story was published in response to ICE’s policy regarding international visa and immigration status amid COVID-19 and school reopening. For this story, I worked on scraping college tuition and student visa data. I also worked on making the map and the bar chart in illustrator and D3geo.',
         },
         {
             imgPath: 'test.jpg',
