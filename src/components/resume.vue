e0a3457676498119a9c3b07222467115cc58d142
Charlotte Li <56002814+shallotly@users.noreply.github.com>
2023-11-21 16:34:43 -0600

    Update resume.vue
    added google drive pdf pop-up

--- src/components/resume.vue
+++ src/components/resume.vue
@@ -8,7 +8,7 @@ defineProps({
 </script>
 
 <template>
-  <div>This page is still under construction :).</div>
+  <div><iframe src="https://drive.google.com/file/d/1aPKg6KDg7BRcOl0qcIYM9r7pfMjsQK_D/preview" width="640" height="480" allow="autoplay"></iframe></div>
 </template>
 
 <style scoped>
