ae8d996c98cf215375ee13242427686124ab48eb
Charlotte Li <56002814+shallotly@users.noreply.github.com>
2023-11-21 16:38:42 -0600

    Update resume.vue

--- src/components/resume.vue
+++ src/components/resume.vue
@@ -8,7 +8,9 @@ defineProps({
 </script>
 
 <template>
-  <div><iframe src="https://drive.google.com/file/d/1aPKg6KDg7BRcOl0qcIYM9r7pfMjsQK_D/preview" width="640" height="480" allow="autoplay"></iframe></div>
+  <div class='resume-container'>
+    <iframe src="https://drive.google.com/file/d/1aPKg6KDg7BRcOl0qcIYM9r7pfMjsQK_D/preview" width="640" height="480" allow="autoplay"></iframe>
+  </div>
 </template>
 
 <style scoped>
