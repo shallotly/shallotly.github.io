93c876ff2e1554415ae682c50dde4796dc47da00
Charlotte Li <56002814+shallotly@users.noreply.github.com>
2024-04-15 11:29:43 -0500

    Update resume.vue
    new CV

--- src/components/resume.vue
+++ src/components/resume.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
   <div class='resume-container'>
-    <iframe src="https://drive.google.com/file/d/1aPKg6KDg7BRcOl0qcIYM9r7pfMjsQK_D/preview" width="640" height="480" allow="autoplay"></iframe>
+    <iframe src="https://drive.google.com/file/d/1Ps6L-MWg5UxKeqCoU8oICZNiPjWnuOrZ/preview" width="640" height="480" allow="autoplay"></iframe>
   </div>
 </template>
 
